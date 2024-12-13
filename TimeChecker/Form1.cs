0335438a878df335722342becc6a8781c8f91e39
Nikon Vitaly <cakevit@gmail.com>
2024-12-13 13:06:44 +0500

    Printed "Hello, World"! It works.

--- TimeChecker/Form1.cs
+++ TimeChecker/Form1.cs
@@ -3,5 +3,9 @@ namespace TimeChecker {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            MessageBox.Show("Hello, World!", "Caption");
+        }
     }
 }
